09e33d417daae14af87bd250b6189b3f140fb38d
Berra Özelgül <berrazelgl@hotmail.com>
2025-12-14 01:40:00 +0300

    FIX: app.js rota sirasi 404 hatasi icin duzeltildi.

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
-require('dotenv').config();  
+require('dotenv').config();  
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 
-
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-require('./app_api/models/db');
-var apiRoute= require('./app_api/routes/index');
+require('./app_api/models/db'); 
+var apiRoute= require('./app_api/routes/index'); 
 var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+
 app.use(express.static(path.join(__dirname, 'public')));
-app.use("/api", apiRoute);
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+app.use("/api", apiRoute);
 
 module.exports = app;
